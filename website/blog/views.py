764f068c8081dc11b796a1666c965d1be088d1de
jaimmart32 <vivalafruta11@gmail.com>
2025-02-03 20:53:33 +0100

    Added comments for posts, tags for posts, django_taggit app. Now can filter posts by tags.

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.conf import settings
+from taggit.models import Tag
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)# 3 per page
     page = request.GET.get('page')
     try:
@@ -17,12 +24,28 @@ def post_list(request):
     except EmptyPage:
         # Out of range page,deliver last
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',
                             publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # List for active comments for post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        # Comment sent
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments,
+                                                     'new_comment': new_comment,
+                                                     'comment_form': comment_form})
 
 class PostListView(ListView):
     queryset = Post.published.all()
